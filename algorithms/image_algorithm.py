eafef0ef4b0e52640e290ed302c7a784b63c7e7e
Ryan Russell <ryanrussell@uvic.ca>
2019-03-30 15:40:42 -0700

    Re-uploading image algorithm files.
    Re-uploading the files in order to put them in the algorithms folder.

---
+++ algorithms/image_algorithm.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageFilter
+from PIL.ExifTags import TAGS
+
+# Read image
+im = Image.open( 'china_image.jpg' )
+
+# Apply multiple filters to the image
+im_edited = im.filter(ImageFilter.SHARPEN)
+im_edited = im.filter(ImageFilter.BLUR)
+im_edited = im.filter(ImageFilter.CONTOUR)	
+
+# Split the image into its respective bands, i.e. Red, Green, and Blue for RGB
+#r,g,b = im_edited.split()
+
+# Save the filtered image to a new file
+im_edited.save('china_image_edited.jpg', 'JPEG')
+
+# Set counter for required EXIF data
+count = 0;
+
+file = open("output.txt", "w")
+
+
+# View EXIF data embedded in image
+ret = {}
+info = im._getexif()
+for tag, value in info.items():
+	decoded = TAGS.get(tag, tag)
+	ret[decoded] = value
+	string =  "{}:{}\n".format(decoded, ret[decoded])
+	file.write(string)
+	count = count + 1
+	if count >= 48:
+		break
